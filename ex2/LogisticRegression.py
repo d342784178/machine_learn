905e0e2b174bbcf7e235f21a15320355d141ad5e
董良剑 <dongliangjian@qccr.com>
2017-06-22 20:10:09 +0800

    ~

--- ex2/LogisticRegression.py
+++ ex2/LogisticRegression.py
@@ -1,7 +1,6 @@
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 from numpy.matlib import repmat
 import math
 
@@ -22,11 +21,10 @@ class Predictor:
         self.times = times
         self.rate = rate
 
-        self._outs_ = []
         self.mu = []
         self.sigma = []
         # 增加一列x0=1
-        # self.x = self._featureScaling_(x)
+        self.x, self.mu, self.sigma = self._featureScaling_(x)
         self.x = np.c_[np.ones((self.x.shape[0], 1)), self.x]
         self.theta = np.zeros([self.x.shape[1], 1])
         self.m = self.x.shape[0]
@@ -36,15 +34,15 @@ class Predictor:
         inputDatas.astype('float64')
         # 特征缩放
         inputDatas = (inputDatas - self.mu) / self.sigma
-        data = np.c_[[[1]], inputDatas].astype('float64')
+        data = np.c_[np.ones([inputDatas.shape[0], 1]), inputDatas].astype('float64')
         return np.dot(data, self.theta)
 
     # 特征缩放
     def _featureScaling_(self, inputDatas):
-        self.mu = np.mean(inputDatas, axis=0)
-        self.sigma = np.std(inputDatas, axis=0)
-        return (inputDatas - repmat(self.mu, np.shape(inputDatas)[0], 1)) / repmat(self.sigma, np.shape(inputDatas)[0],
-                                                                                   1)
+        mu = np.mean(inputDatas, axis=0)
+        sigma = np.std(inputDatas, axis=0)
+        return (inputDatas - repmat(mu, np.shape(inputDatas)[0], 1)) / repmat(sigma, np.shape(inputDatas)[0],
+                                                                              1), mu, sigma
 
     def train(self):
         for i in range(self.times):
@@ -63,7 +61,6 @@ class Predictor:
         grad = self.x.T.dot((h - self.y)) / self.m
         self.theta -= self.rate * grad
         print('grad', grad)
-        self._outs_.append(h)
         # print('weight:', self._weight_)
 
 
@@ -74,7 +71,23 @@ if __name__ == '__main__':
     plt.plot(loadtxt[zero][:, 0], loadtxt[zero][:, 1], 'r+')
     plt.plot(loadtxt[one][:, 0], loadtxt[one][:, 1], 'b.')
     # plt.show()
-    predictor = Predictor(np.transpose(np.matrix(loadtxt[:, 2])), np.matrix(loadtxt[:, 0:2]), 1000, 0.01)
+    Y = np.transpose(np.matrix(loadtxt[:, 2]))
+    X = np.matrix(loadtxt[:, 0:2])
+    predictor = Predictor(Y, X, 1000, 0.1)
     predictor.train()
-    print(sigmoid(predictor.getResult(np.matrix([45, 85]))))
-    plt.plot()
+    print('result', sigmoid(predictor.getResult(np.matrix([45, 85]))))
+    # ys = []
+    # for i in predictor._featureScaling_(np.matrix(loadtxt[:, 0]).T)[0]:
+    #     ys.append((predictor.theta.T[0, 0] + predictor.theta.T[0, 1] * i[0, 0]) / -predictor.theta.T[0, 2])
+    #
+    # print(ys)
+    # # print(x)
+    # sigma_ = np.matrix(ys).T * predictor.mu[0, 1] + predictor.sigma[0, 1]
+    # print(sigma_.T)
+    # plt.plot(loadtxt[:, 0], sigma_)
+    plot_x = (np.matrix([np.min(X[:, 1]) - 2, np.max(X[:, 1]) + 2]) - predictor.sigma[0, 0]) / predictor.mu[0, 0]
+    print([np.min(X[:, 1]) - 2, np.max(X[:, 1]) + 2])
+    plot_y = (-1. / predictor.theta[2]) * (predictor.theta[1] * (plot_x) + predictor.theta[0])
+    print(plot_y * predictor.mu[0, 1] + predictor.sigma[0, 1])
+    plt.plot([28.60326323428011, 100.86943574220611], [-21.54564318, -100.55773148])
+    plt.show()
