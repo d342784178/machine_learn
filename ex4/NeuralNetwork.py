e22a4695f7a2391293f20c6193bbd411a607c45a
董良剑 <dongliangjian@qccr.com>
2017-06-28 22:48:30 +0800

    ~

--- ex4/NeuralNetwork.py
+++ ex4/NeuralNetwork.py
@@ -6,12 +6,26 @@
 __author__ = 'DLJ'
 
 import numpy as np
-import datetime
 import matplotlib.pyplot as plt
 from numpy.matlib import repmat
 import scipy.io as sio
 from PIL import Image as img
 from scipy.optimize import fmin_tnc
+import os
+import time
+
+
+def exeTime(func):
+    """ 耗时计算装饰器
+    """
+
+    def newFunc(*args, **args2):
+        t0 = time.time()
+        back = func(*args, **args2)
+        print('耗时', func.__name__, time.time() - t0)
+        return back
+
+    return newFunc
 
 
 def sigmoid(z):
@@ -22,15 +36,16 @@ def sigmoidGradient(z):
     return np.multiply(sigmoid(z), (1 - sigmoid(z)))
 
 
+@exeTime
 def costFunction(X, y, theta, input_layer_size, hidden_layer_size, out_layer_size, m, lmd):
     Y = np.zeros([m, out_layer_size])
     for ii in range(m):
         Y[ii, y[ii, 0] - 1] = 1
     # 向前推导
-    theta_1 = np.matrix(theta[0, 0:hidden_layer_size * (input_layer_size + 1)]).reshape(
-            hidden_layer_size, (input_layer_size + 1))
-    theta_2 = np.matrix(theta[0, hidden_layer_size * (input_layer_size + 1)::]).reshape(
-            out_layer_size, (hidden_layer_size + 1))
+    theta_1 = np.asmatrix(theta[0, 0:hidden_layer_size * (input_layer_size + 1)]).reshape(
+        hidden_layer_size, (input_layer_size + 1))
+    theta_2 = np.asmatrix(theta[0, hidden_layer_size * (input_layer_size + 1)::]).reshape(
+        out_layer_size, (hidden_layer_size + 1))
 
     X = X.reshape(m, input_layer_size)
     a1 = X
@@ -95,7 +110,7 @@ def checkGradient(lmd):
     X = debugInitWeight(m, input_layer_size)
     y = (np.ones([m, 1]) * out_layer_size).astype('int32')
 
-    theta = np.matrix(np.r_[Theta1.ravel().T, Theta2.ravel().T])
+    theta = np.asmatrix(np.r_[Theta1.ravel().T, Theta2.ravel().T])
 
     espilon = 1e-4
     gradApprox = np.zeros([1, theta.shape[1]])
@@ -104,9 +119,10 @@ def checkGradient(lmd):
         right = np.matrix(theta)
         left[0, i] = theta[0, i] + espilon
         right[0, i] = theta[0, i] - espilon
-        cost_left, grad_left = costFunction(np.matrix(X), y, left, input_layer_size, hidden_layer_size, out_layer_size,
+        cost_left, grad_left = costFunction(np.asmatrix(X), y, left, input_layer_size, hidden_layer_size,
+                                            out_layer_size,
                                             m, lmd)
-        cost_right, grad_right = costFunction(np.matrix(X), y, right, input_layer_size, hidden_layer_size,
+        cost_right, grad_right = costFunction(np.asmatrix(X), y, right, input_layer_size, hidden_layer_size,
                                               out_layer_size, m,
                                               lmd)
         espilon_ = (cost_left - cost_right) / (2 * espilon)
@@ -119,6 +135,27 @@ def checkGradient(lmd):
     delta = np.sum(np.square(gradApprox - grad))
     # print(delta)
     return delta < 1e-9
+
+
+def predict(X, y, theta, input_layer_size, hidden_layer_size, out_layer_size, m, lmd):
+    Y = np.zeros([m, out_layer_size])
+    for ii in range(m):
+        Y[ii, y[ii, 0] - 1] = 1
+    # 向前推导
+    theta_1 = np.asmatrix(theta[0, 0:hidden_layer_size * (input_layer_size + 1)]).reshape(
+        hidden_layer_size, (input_layer_size + 1))
+    theta_2 = np.asmatrix(theta[0, hidden_layer_size * (input_layer_size + 1)::]).reshape(
+        out_layer_size, (hidden_layer_size + 1))
+
+    X = X.reshape(m, input_layer_size)
+    a1 = X
+    a2 = sigmoid(np.c_[np.ones([m, 1]), a1].dot(theta_1.T))
+    a3 = sigmoid(np.c_[np.ones([m, 1]), a2].dot(theta_2.T))
+    return np.where(a3 > 0.5)[1] + 1
+
+
+def checkResult(X, y, theta, input_layer_size, hidden_layer_size, out_layer_size, m, lmd):
+    pass
 
 
 if __name__ == '__main__':
@@ -134,12 +171,16 @@ if __name__ == '__main__':
     print('dataSize:', X.shape)
     m = X.shape[0]
     print('m', m)
-    data2 = sio.loadmat('ex4weights.mat')
+    # data2 = sio.loadmat('ex4weights.mat')
     # theta_1 = data2['Theta1']
     # theta_2 = data2['Theta2']
-    theta_1 = initTheta(input_layer_size, hidden_layer_size)
-    theta_2 = initTheta(hidden_layer_size, out_layer_size)
-    theta = np.matrix(np.r_[theta_1.ravel(), theta_2.ravel()])
+    theta = None
+    if os.path.exists('./theta.mat'):
+        theta = sio.loadmat('./theta.mat')['theat']
+    else:
+        theta_1 = initTheta(input_layer_size, hidden_layer_size)
+        theta_2 = initTheta(hidden_layer_size, out_layer_size)
+        theta = np.matrix(np.r_[theta_1.ravel(), theta_2.ravel()])
 
     image = img.fromarray(X[1000, :].reshape(20, 20).T * 255)
     # image.show()
@@ -149,18 +190,20 @@ if __name__ == '__main__':
     #                                out_layer_size, m, lmd=1)
     # print(J)
     # print(grad)
-    rate = 0.08
-    trainTimes = 1000
-    for i in range(trainTimes):
-        J, grad = costFunction(X, y, theta, input_layer_size, hidden_layer_size,
-                               out_layer_size, m, lmd=1)
-        print('J', J)
-        theta -= rate * grad
-        sio.savemat('/Users/mic/PycharmProjects/machineLearn/ex4/theta.mat', {'theat': theta})
-    print(theta)
-    # tnc = fmin_tnc(costFunction, x0=X, args=(y, theta, input_layer_size, hidden_layer_size, out_layer_size, m, 1),
-    #                approx_grad=True, epsilon=1e-4, )
-    # print(tnc)
-    sio.savemat('/Users/mic/PycharmProjects/machineLearn/ex4/theta.mat', {'theat': theta})
-    # image = img.fromarray(X[1000, :].reshape(20, 20).T * 255)
+
+    # rate = 1
+    # trainTimes = 5000
+    # for i in range(trainTimes):
+    #     J, grad = costFunction(X, y, theta, input_layer_size, hidden_layer_size,
+    #                            out_layer_size, m, lmd=1)
+    #     print('J', J)
+    #     theta -= rate * grad
+    #     sio.savemat('./theta.mat', {'theat': theta})
+    # print(theta)
+    # sio.savemat('./theta.mat', {'theat': theta})
+
+    # image = img.fromarray(X[0:1000, :].reshape(20, 20).T * 255)
     # image.show()
+    result = predict(X[0:50, :], y, theta, input_layer_size, hidden_layer_size, out_layer_size, m=50, lmd=1)
+    print(result)
+    # print(np.matrix(result).T - y[0:1000, :])
